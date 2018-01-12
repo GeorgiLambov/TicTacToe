0182646fe7d4c4d4ad802b610431db204bcb8b5b
GeorgiLambov <g_lambov@bitex.bg>
2018-01-12 14:37:43 +0200

    add index.html, package.json and Gruntfile,

---
+++ src/Gruntfile.js
@@ -0,0 +1,95 @@
+/**
+ * Created by g.lambov on 12.01.2018.
+ */
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        browserify: {
+            dist: {
+                options: {
+                    transform: [
+                        [
+                            "babelify",
+                            {
+                                presets: ["es2015", "react"]
+                            }
+                        ]
+                    ]
+                },
+                files: {
+                    "./public/App.js": ["./src/js/Client.js"]
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ["./src/**/*.js"],
+                tasks: ["browserify"]
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    keepalive: true,
+                    livereload: true,
+                    base: {
+                        path: "public/",
+                        options: {
+                            index: "index.html"
+                        }
+                    }
+                }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    "public/main.css": "src/client.scss"
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: "src/**/*.js",
+                tasks: ["browserify"]
+            },
+            css: {
+                files: "src/**/*.scss",
+                tasks: ["sass"]
+            },
+            html: {
+                files: "src/**/*.html",
+                tasks: ["copy"]
+            }
+        },
+        copy: {
+            html: {
+                expand: true,
+                flatten: true,
+                src: ["**/*.html"],
+                cwd: "src/",
+                dest: "public/",
+                filter: "isFile"
+            }
+        },
+        concurrent: {
+            options: {
+                logConcurrentOutput: true,
+                limit: 10
+            },
+            dev: {
+                tasks: ["browserify", "connect", "watch"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-browserify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-connect");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-concurrent");
+    grunt.loadNpmTasks("grunt-sass");
+
+    grunt.registerTask("default", ["copy", "sass", "concurrent"]);
+};
